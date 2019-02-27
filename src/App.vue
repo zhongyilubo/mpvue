852f29a3b0bd988d77e69f9fd212e7bccbfb734b
duanzhiwei <89340545@qq.com>
2019-02-27 18:12:06 +0800

    df

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+  created () {
+    // 调用API从本地缓存中获取数据
+    /*
+     * 平台 api 差异的处理方式:  api 方法统一挂载到 mpvue 名称空间, 平台判断通过 mpvuePlatform 特征字符串
+     * 微信：mpvue === wx, mpvuePlatform === 'wx'
+     * 头条：mpvue === tt, mpvuePlatform === 'tt'
+     * 百度：mpvue === swan, mpvuePlatform === 'swan'
+     * 支付宝(蚂蚁)：mpvue === my, mpvuePlatform === 'my'
+     */
+
+    let logs
+    if (mpvuePlatform === 'my') {
+      logs = mpvue.getStorageSync({key: 'logs'}).data || []
+      logs.unshift(Date.now())
+      mpvue.setStorageSync({
+        key: 'logs',
+        data: logs
+      })
+    } else {
+      logs = mpvue.getStorageSync('logs') || []
+      logs.unshift(Date.now())
+      mpvue.setStorageSync('logs', logs)
+    }
+
+
+      // 登录
+      wx.login({
+          success: res => {
+              // 发送 res.code 到后台换取 openId, sessionKey, unionId
+              console.log(res);
+          }
+      })
+      // 获取用户信息
+      wx.getSetting({
+          success: res => {
+              if (res.authSetting['scope.userInfo']) {
+                  // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                  wx.getUserInfo({
+                      success: res => {
+                          // 可以将 res 发送给后台解码出 unionId
+                          mpvue.setStorageSync('userInfo', res.userInfo);
+
+                          // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                          // 所以此处加入 callback 以防止这种情况
+                          if (this.userInfoReadyCallback) {
+                              this.userInfoReadyCallback(res)
+                          }
+                      }
+                  })
+              }
+          }
+      })
+
+
+  },
+  log () {
+    console.log(`log at:${Date.now()}`)
+  },
+    globalData: {
+        userInfo: null
+    }
+}
+</script>
+
+<style>
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 200rpx 0;
+  box-sizing: border-box;
+}
+/* this rule will be remove */
+* {
+  transition: width 2s;
+  -moz-transition: width 2s;
+  -webkit-transition: width 2s;
+  -o-transition: width 2s;
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,167 @@
+<template>
+  <div @click="clickHandle">
+
+    <button open-type="getUserInfo" @getuserinfo="getUserInfo"> 获取头像昵称 </button>
+
+    <div class="userinfo" @click="bindViewTap">
+      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
+      <img class="userinfo-avatar" v-if="!userInfo.avatarUrl" src="/static/images/user.png" background-size="cover" />
+
+      <div class="userinfo-nickname">
+        <card :text="userInfo.nickName"></card>
+      </div>
+    </div>
+
+    <div class="usermotto">
+      <div class="user-motto">
+        <card :text="motto"></card>
+      </div>
+    </div>
+
+    <form class="form-container">
+      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
+      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
+      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
+    </form>
+
+    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
+
+    <div class="all">
+        <div class="left">
+        </div>
+        <div class="right">
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello miniprograme',
+      hasUserInfo: false,
+      canIUse: wx.canIUse('button.open-type.getUserInfo'),
+      userInfo: {
+        nickName: mpvue.getStorageSync('userInfo')['nickName'],
+        avatarUrl: mpvue.getStorageSync('userInfo')['avatarUrl']
+      }
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      if (mpvuePlatform === 'wx') {
+        mpvue.switchTab({ url })
+      } else {
+        mpvue.navigateTo({ url })
+      }
+    },
+    clickHandle (ev) {
+      console.log('clickHandle:', ev)
+      // throw {message: 'custom test'}
+    },
+    getUserInfo: function(e) {
+        let app = getApp()
+
+        console.log(e)
+          // app.globalData.userInfo = e.detail.userInfo
+          // this.setData({
+          //     userInfo: e.detail.userInfo,
+          //     hasUserInfo: true
+          // })
+      }
+  },
+
+  created () {
+      let app = getApp()
+
+      if (app.globalData.userInfo) {
+          this.setData({
+              userInfo: app.globalData.userInfo,
+              hasUserInfo: true
+          })
+      } else if (this.canIUse){
+          // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+          // 所以此处加入 callback 以防止这种情况
+          app.userInfoReadyCallback = res => {
+              this.setData({
+                  userInfo: res.userInfo,
+                  hasUserInfo: true
+              })
+          }
+      } else {
+          // 在没有 open-type=getUserInfo 版本的兼容处理
+          wx.getUserInfo({
+              success: res => {
+                  app.globalData.userInfo = res.userInfo
+                  this.setData({
+                      userInfo: res.userInfo,
+                      hasUserInfo: true
+                  })
+              }
+          })
+      }
+  }
+}
+</script>
+
+<style scoped>
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 128rpx;
+  height: 128rpx;
+  margin: 20rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+
+.usermotto {
+  margin-top: 150px;
+}
+
+.form-control {
+  display: block;
+  padding: 0 12px;
+  margin-bottom: 5px;
+  border: 1px solid #ccc;
+}
+.all{
+  width:7.5rem;
+  height:1rem;
+  background-color:blue;
+}
+.all:after{
+  display:block;
+  content:'';
+  clear:both;
+}
+.left{
+  float:left;
+  width:3rem;
+  height:1rem;
+  background-color:red;
+}
+
+.right{
+  float:left;
+  width:4.5rem;
+  height:1rem;
+  background-color:green;
+}
+</style>
