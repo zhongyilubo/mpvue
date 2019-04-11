5a33b34946c12185107b49896e66ae3b86d4457f
duan <1250101915@qq.com>
2019-04-11 22:09:22 +0800

    df

--- src/pages/comment/index.vue
+++ src/pages/comment/index.vue
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+  import '@/assets/css/comment.css';
   export default {
     data: {
       message: 'Hello Vue!'
@@ -19,80 +20,4 @@
       }
     }
   }
-
 </script>
-
-<style scoped>
-
-  /* 通用 */
-  h1{ font-size: 42rpx; color: #333;}
-  p{ font-size: 30rpx; color: #000;}
-  .red{ color:#d11208; }
-  .gray{ color:#666;}
-  .h130{ height: 130rpx;}
-
-  /* 钱 名字 */
-  .zt-money{ overflow: hidden; font-size: 26rpx; margin-top: 15rpx;}
-  .zt-money dt{ float: left}
-  .zt-money dd{ float: right}
-
-  .myContainer{ width: 92%; margin:0 4%;}
-  /*--------------------------------------------*/
-  .inp-search{
-    width: 100%;
-    height: 55rpx;
-    font-size: 30rpx;
-    line-height: 30rpx;
-    border: 1px solid #8781a6;
-    display: inline-block;
-    box-sizing: border-box;
-    text-indent: 5px;
-    border-radius: 8rpx;
-    padding:0 0 0 70rpx;
-    margin: 13rpx 0 15rpx;
-    background: url("../../../../mpvue/static/images/search.png") no-repeat;
-    background-size:33rpx;
-    background-position: 20rpx center ;
-  }
-  .banner img{
-    width: 100%;
-    height: 350rpx;
-    /*background:blue;*/
-  }
-  .myTitle{
-    margin: 25rpx 0;
-  }
-  .zt-box{
-    overflow: hidden;
-  }
-  .zt-box li{ margin-bottom: 15px;}
-  .zt-box li img{ width: 100%; height: auto;}
-  .zt-box li:nth-child(2n-1){
-    float: left;
-    width: calc(50% - 8rpx);
-    margin-right: 8rpx;
-  }
-  .zt-box li:nth-child(2n){
-    float: left;
-    width: calc(50% - 8rpx);
-    margin-left: 8rpx;
-  }
-  /* nav 导航 */
-  .nav{
-    position:fixed;
-    bottom: 0;
-    width: 100%;
-    font-size: 30rpx;
-    padding:20rpx 0;
-    background: #fff;
-    border-top: 1rpx solid #e6e6e6;
-    overflow: hidden;
-  }
-  .nav li{
-    width: 25%;
-    text-align: center;
-    float:left;
-  }
-  .nav img{ width: 40rpx;}
-
-</style>
