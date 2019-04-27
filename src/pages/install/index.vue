aea47bbcdb8370aaa34856d9405aa44b58935a2f
duan <1250101915@qq.com>
2019-04-27 21:23:23 +0800

    beifen

--- src/pages/install/index.vue
+++ src/pages/install/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <ul class="install-list">
-      <li class="change">消息通知<div><span class="open"></span></div></li>
-      <li>清除缓存</li>
+      <li class="change"><!--消息通知-->{{box1}}
+        <div>
+          <!-- <span class="open"></span> v-bind:class="(_this.box1 === 1)? 'open':' close ' " -->
+          <span :class="activeClass == index ? 'open':''" v-for="(item,index) in itemList" :key="index" @click="getItem(index)"></span>
+
+
+        </div>
+      </li>
+      <li @click='qingchu'>清除缓存</li>
       <li>退出登录</li>
     </ul>
   </div>
@@ -12,12 +19,45 @@
   import '@/assets/css/install.css';
   export default {
     data: {
-      message: 'Hello Vue!'
+      box1: '123456',
+      return{
+        activeClass: -1, // 0为默认选择第一个，-1为不选择
+      }
+    },
+    mounted(){
+      this.changebut()
     },
     methods: {
-      dddd(){
-        this.message = 'asdfsadfsdaf'
+      qingchu(){
+        var _this = this;
+        _this.$net.post({
+          url:'clearcatch',
+          data:{}
+        }).then(res =>{
+          _this.qingchu.value = res.data.zhuanfa;
+        //console.log(res.message)
+        })
+      },
+      changebut(){
+        var _this = this;
+            _this.$net.post({
+              url:'message',
+              data:{
+              }
+            }).then(res =>{
+              _this.box1 = res.data.type;
+            })
+          console.log(  )
+        /*if(_this.message === 0){
+              $('.change>span').className('red')
+        }else if(_this.message === 1){
+
+        }*/
+        getItme(index){
+          this.activeClass = index;  // 把当前点击元素的index，赋值给activeClass
+        }
       }
+
     }
   }
 </script>
