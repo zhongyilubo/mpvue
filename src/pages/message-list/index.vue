c3ad1baee9aea2de5ad9a61e379f4bf060d1bfac
duan <1250101915@qq.com>
2019-05-02 13:14:37 +0800

    页面

--- src/pages/message-list/index.vue
+++ src/pages/message-list/index.vue
@@ -6,24 +6,16 @@
         <p>尊敬的用户，您于2019年12月21日，成功支付了52. 00元，并获得了，52积分，请在我的积分中查看</p>
         <dl class="zt-money gray">
           <dt class="kuan">201903-31 23:59:59</dt>
-          <!--<dd class="red">删除</dd>-->
+          <dd class="red">查看详情</dd>
         </dl>
       </li>
       <li>
-        <h1 class="myTitle">商品明细</h1>
-        <p>著名中医教你如何正确的拔罐子...</p>
+        <h1 class="myTitle">系统消息</h1>
+        <p>尊敬的用户，您于2019年12月21日，成功支付了52. 00元，并获得了，52积分，请在我的积分中查看</p>
         <dl class="zt-money gray">
-          <dt class="kuan"><span>李时珍</span>&nbsp;&nbsp;<span>系列课</span></dt>
-          <dd class="red">¥:<span>52.00</span></dd>
+          <dt class="kuan">201903-31 23:59:59</dt>
+          <dd class="red">查看详情</dd>
         </dl>
-      </li>
-      <li>
-        <h1 class="myTitle">支付方式</h1>
-        <p>微信支付</p>
-      </li>
-      <li>
-        <p class="mt-25">订单编号：<span>HF58988574</span></p>
-        <p class="mt-25">下单时间：<span>2019-03-31 23:59:49</span></p>
       </li>
     </ul>
   </div>
