e739d1148e3b2c578e4dd0bb9eeef4e6d0669071
duanzhiwei <89340545@qq.com>
2019-05-14 18:11:43 +0800

    df

--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -6,21 +6,21 @@
       <h1 class="myTitle">购买内容</h1>
       <ul class="zt-box zt-box-img">
         <li>
-          <span><img src="../../../../mpvue/static/images/class-img1.png" alt="" ></span>
+          <span><img :src="cover" alt="" ></span>
         </li>
         <li>
-          <p>著名中医教你如何正确的拔罐子</p>
+          <p class="mmmmm">{{name}}</p>
           <dl class="zt-money gray">
             <dt></dt>
-            <dd>03-31</dd>
+            <dd>{{date}}</dd>
           </dl>
           <dl class="zt-money gray">
-            <dt>李时珍</dt>
-            <dd><span>256</span>次</dd>
+            <dt>{{teacher}}</dt>
+            <dd><span>{{number}}</span>次</dd>
           </dl>
           <dl class="zt-money red">
-            <dt>￥:<span>52.00</span></dt>
-            <dd>系列课</dd>
+            <dt><span>{{pay_view}}</span></dt>
+            <dd>{{type_name}}</dd>
           </dl>
         </li>
       </ul>
@@ -29,8 +29,8 @@
 
     <div class="h130"></div>
     <dl class="weixin">
-      <dt>共计：￥<span>50.00</span></dt>
-      <dd>去支付</dd>
+      <dt>共计：<span>{{pay_view}}</span></dt>
+      <dd @click="topay">去支付</dd>
     </dl>
 
   </div>
@@ -41,7 +41,24 @@
   import '@/assets/css/style.css';
   export default {
     data: {
-        id: 0
+        id: 0,
+        name: '',
+        type_name: '',
+        type: '',
+        video: '',
+        number: '',
+        teacher: '',
+        date: '',
+        timer: '',
+        intro: '',
+        pay: 2,
+        pay_name: '',
+        pay_view: '',
+        sons: '',
+        cover: '',
+        title:'中推在线',
+        ispay:0,
+        isshare:0,
     },
     mounted(){
       var _this = this;
@@ -49,10 +66,23 @@
       this.id = this.getQuery().id;//接收这个参数 jq mpvue接收的方式不一样
 
       _this.$net.post({
-          url: 'payinfo/'+_this.id,
+          url: 'detail/'+_this.id,
           data: {}
       }).then(res => {
-
+          _this.name = res.data.name;
+          _this.type_name = res.data.type_name;
+          _this.type = res.data.type;
+          _this.teacher = res.data.teacher;
+          _this.name = res.data.name;
+          _this.number = res.data.number;
+          _this.date = res.data.date;
+          _this.timer = res.data.timer;
+          _this.video = res.data.video;
+          _this.cover = res.data.cover;
+          _this.intro = res.data.intro;
+          _this.pay = res.data.pay;
+          _this.pay_name = res.data.pay_name;
+          _this.pay_view = res.data.pay_view;
       })
     },
     methods: {
@@ -63,7 +93,21 @@
             const options = currentPage.options
             return options
         },
+        topay(){
+            var _this = this;
+
+            _this.$net.post({
+                url: 'pay/'+_this.id,
+                data: {}
+            }).then(res => {
+
+            })
+        }
     }
   }
 </script>
-
+<style>
+  .mmmmm{
+    min-height: 87rpx;
+  }
+</style>
